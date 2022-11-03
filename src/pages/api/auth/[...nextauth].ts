082c8fba8c65c0745168a5509103a82d13c6bf61
Gian Lucas <lgian584@gmail.com>
2022-11-03 00:15:56 -0300

    add axios and save user with login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,33 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { SignInOptions } from "next-auth/react";
+import { api } from "../../../lib/axios";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const { data } = await api.get(`/users/${user.email}`);
+
+        if (!data.user) {
+          await api.post("/users", {
+            email: user.email,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 };
 
 export default NextAuth(authOptions);
